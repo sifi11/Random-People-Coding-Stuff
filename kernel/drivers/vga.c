5c56c6b83fa5610fc8c536ad6cb1bf15388d9913
Olen T <elroylilly@gmail.com>
2026-03-20 15:05:30 -0400

    Update vga.c

--- kernel/drivers/vga.c
+++ kernel/drivers/vga.c
@@ -1,1 +1,12 @@
+#include "vga.h"
 
+int puts(char* str) // totally not stolen from crystal, lol
+{
+	volatile char* pVgaTextBuff = 0xB8000;
+	for (str; *str != 0; str++ )
+        {
+        	pVgaTextBuff[0] = *str;
+        	pVgaTextBuff[1] = 0x0f; //color atrib
+        	pVgaTextBuff += 2;
+        }
+}
